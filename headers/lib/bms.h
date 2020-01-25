f3f567bc8926220c3b572c0794bb528eb45c9a0f
mrtyvz41 <41593973+mrtyvz41@users.noreply.github.com>
2020-01-25 22:46:34 +0300

    just getting new attribute

--- headers/lib/bms.h
+++ headers/lib/bms.h
@@ -2,9 +2,17 @@
 #define BMS_H
 struct BMS
 {
-        
+        char bufBms[200];
+        char rcv_ch;
+        unsigned short bufIxBms = 0;
+        //void (*getUartData)(char);
+        //void (*parseUartData)(char*)
 };
+typedef struct BMS* Bms;
+Bms Init_Bms( void );
 
+//void _getUartData(char, unsigned short);
+//void _parseUartData(char*);
 
 
 #endif
--- headers/src/bms.c
+++ headers/src/bms.c
@@ -1,1 +1,15 @@
 #include "headers/lib/bms.h"
+#include <stdlib.h>
+#include <stdio.h>
+Bms Init_Bms( void ){
+    Bms thiss;
+    thiss = (Bms)malloc(sizeof(struct Bms));
+    thiss->getUartData = &_getUartData;
+    thiss->parseUartData = &_parseUartData;
+
+    return thiss;
+}
+
+//void _getUartData(char rcv_ch){
+    
+}
